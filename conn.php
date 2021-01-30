05dddd4607b90491f36ce8b4bb29fcd7e731141a
NAVI-AI <73737137+NAVI-AI@users.noreply.github.com>
2021-01-30 15:41:28 +0000

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = "localhost";
 $username = "root";
-$password = "";
-$conn = mysql_connect($dbhost,$username,$password) or die(mysql_error());
+$password = "yanbaojie000";
+$conn = mysql_connect($dbhost,$username,$password) or die(mysql_error(3157426612));
 mysql_query('set names utf8',$conn);
-mysql_select_db('gps',$conn) or die(mysql_error());
+mysql_select_db('gps',$conn) or die(mysql_error(3157426612));
